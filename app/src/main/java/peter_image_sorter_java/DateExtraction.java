dbfa0598b7ca71a0ff587135bd13f7c2d4a7f4c7
trekimann <10469075+trekimann@users.noreply.github.com>
2022-05-16 15:00:13 +0100

    change to Calendar. Add mp4

--- app/src/main/java/peter_image_sorter_java/DateExtraction.java
+++ app/src/main/java/peter_image_sorter_java/DateExtraction.java
@@ -4,23 +4,26 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.exif.ExifSubIFDDirectory;
+import com.drew.metadata.mp4.Mp4Directory;
 
 public class DateExtraction {
-    public Date getFileDateTaken(String fileLocation){
+    public Calendar getFileDateTaken(String fileLocation){
         var fileType = fileLocation.substring(fileLocation.lastIndexOf(".") + 1);
-        Date toReturn = null;
+        Calendar toReturn = null;
         switch (fileType) {
             case "jpg":
             case "heic":
                 // image files, use image date getter
                 try {
-                    toReturn = getDateTaken(fileLocation);
+                    toReturn = getDateTakenForImage(fileLocation);
                 } catch (ImageProcessingException e) {
                     e.printStackTrace();
                 } catch (IOException e) {
@@ -28,6 +31,15 @@ public class DateExtraction {
                 }
                 break;
             case "mp4":
+                try {
+                    toReturn = getDateTakenForMp4(fileLocation);
+                } catch (ImageProcessingException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
                 // mp4 file, use video data
                 break;
             default:
@@ -36,14 +48,41 @@ public class DateExtraction {
         return toReturn;
     }
 
-    public Date getDateTaken(String fileLocation) throws ImageProcessingException, IOException{
+    public Calendar getDateTakenForImage(String fileLocation) throws ImageProcessingException, IOException{
         String imageToRead = new File(fileLocation).getAbsolutePath();
-        InputStream imageStream = new FileInputStream(imageToRead);
-        Metadata imageMetaData = ImageMetadataReader.readMetadata(imageStream);
+        InputStream inputStream = new FileInputStream(imageToRead);
+        Metadata imageMetaData = ImageMetadataReader.readMetadata(inputStream);
 
         var directory = imageMetaData.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
+        
         Date date = directory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
 
-        return date;
+        // only care about the date, not the time so force that here
+        return setToMidnight(calendar);
+    }
+
+    public Calendar getDateTakenForMp4(String fileLocation) throws ImageProcessingException, IOException{
+        String videoToRead = new File(fileLocation).getAbsolutePath();
+        InputStream inputStream = new FileInputStream(videoToRead);
+        Metadata imageMetaData = ImageMetadataReader.readMetadata(inputStream);
+
+        var directory = imageMetaData.getFirstDirectoryOfType(Mp4Directory.class);
+        
+        Date date = directory.getDate(Mp4Directory.TAG_CREATION_TIME);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        // only care about the date, not the time so force that here
+        return setToMidnight(calendar);
+    }
+
+    private Calendar setToMidnight(Calendar calendar){
+        calendar.set(Calendar.HOUR_OF_DAY,0);
+        calendar.set(Calendar.MINUTE,0);
+        calendar.set(Calendar.SECOND,0);
+
+        return calendar;
     }
 }
--- app/src/test/java/peter_image_sorter_java/DateExtractionTest.java
+++ app/src/test/java/peter_image_sorter_java/DateExtractionTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import com.drew.imaging.ImageProcessingException;
@@ -18,41 +19,45 @@ public class DateExtractionTest {
 
     DateExtraction dateExtraction;
     String test_jpg_location = "src/test/resources/test_jpg.jpg";
-    String dateJpgWasTaken = "27-02-2019 13:13:35";
+    String dateJpgWasTaken = "27-02-2019";
     
     String test_heic_location = "src/test/resources/test_heic.heic";
-    String dateHeicWasTaken = "22-01-2022 12:08:40 pm";
+    String dateHeicWasTaken = "12-10-2021";
     
     String test_mp4_location = "src/test/resources/test_mp4.mp4";
-    String dateMp4WasTaken = "27-02-2019 13:13:35";
+    String dateMp4WasTaken = "29-03-2021";
+
+    Calendar expected;
+
     @BeforeEach
     void setup(){
         dateExtraction = new DateExtraction();
+        expected = Calendar.getInstance();
     }
     
     @Test void whenJpgHasMetadataThenDateShouldBeExtracted() throws ImageProcessingException, IOException, ParseException{
-        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");   
-        Date expected = formatter.parse(dateJpgWasTaken);
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");   
+        expected.setTime(formatter.parse(dateJpgWasTaken));
 
-        var actual = dateExtraction.getDateTaken(test_jpg_location);
+        var actual = dateExtraction.getDateTakenForImage(test_jpg_location);
 
         assertEquals(expected, actual);
     }
 
     @Test void whenHeicHasMetadataThenDateShouldBeExtracted() throws ImageProcessingException, IOException, ParseException{
-        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss aa");   
-        Date expected = formatter.parse(dateHeicWasTaken);
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");   
+        expected.setTime(formatter.parse(dateHeicWasTaken));
 
-        var actual = dateExtraction.getDateTaken(test_heic_location);
+        var actual = dateExtraction.getDateTakenForImage(test_heic_location);
 
         assertEquals(expected, actual);
     }
 
     @Test void whenMp4HasMetadataThenDateShouldBeExtracted() throws ImageProcessingException, IOException, ParseException{
-        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");   
-        Date expected = formatter.parse(dateMp4WasTaken);
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");   
+        expected.setTime(formatter.parse(dateMp4WasTaken));
 
-        var actual = dateExtraction.getDateTaken(test_mp4_location);
+        var actual = dateExtraction.getDateTakenForMp4(test_mp4_location);
 
         assertEquals(expected, actual);
     }
