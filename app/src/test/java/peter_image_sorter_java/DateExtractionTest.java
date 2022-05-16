9a0a16c453f6ed995f8c7aa58b31f40aea5805b5
trekimann <10469075+trekimann@users.noreply.github.com>
2022-05-16 13:16:25 +0100

    add mp4 test

--- app/src/test/java/peter_image_sorter_java/DateExtractionTest.java
+++ app/src/test/java/peter_image_sorter_java/DateExtractionTest.java
@@ -10,7 +10,6 @@ import java.util.Date;
 
 import com.drew.imaging.ImageProcessingException;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
 import peter_image_sorter_java.DateExtraction;
@@ -18,13 +17,14 @@ import peter_image_sorter_java.DateExtraction;
 public class DateExtractionTest {
 
     DateExtraction dateExtraction;
-    // String test_jpg_location = "C:\\Users\\treki\\source\\Java\\Peter_Image_Sorter_java\\app\\src\\test\\resources\\test_jpg.jpg";
     String test_jpg_location = "src/test/resources/test_jpg.jpg";
     String dateJpgWasTaken = "27-02-2019 13:13:35";
     
     String test_heic_location = "src/test/resources/test_heic.heic";
-    String dateHeicWasTaken = "27-02-2019 13:13:35";
-
+    String dateHeicWasTaken = "22-01-2022 12:08:40 pm";
+    
+    String test_mp4_location = "src/test/resources/test_mp4.mp4";
+    String dateMp4WasTaken = "27-02-2019 13:13:35";
     @BeforeEach
     void setup(){
         dateExtraction = new DateExtraction();
@@ -34,16 +34,25 @@ public class DateExtractionTest {
         SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");   
         Date expected = formatter.parse(dateJpgWasTaken);
 
-        var actual = dateExtraction.getImageDateTaken(test_jpg_location);
+        var actual = dateExtraction.getDateTaken(test_jpg_location);
 
         assertEquals(expected, actual);
     }
 
     @Test void whenHeicHasMetadataThenDateShouldBeExtracted() throws ImageProcessingException, IOException, ParseException{
-        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");   
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss aa");   
         Date expected = formatter.parse(dateHeicWasTaken);
 
-        var actual = dateExtraction.getImageDateTaken(test_heic_location);
+        var actual = dateExtraction.getDateTaken(test_heic_location);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test void whenMp4HasMetadataThenDateShouldBeExtracted() throws ImageProcessingException, IOException, ParseException{
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");   
+        Date expected = formatter.parse(dateMp4WasTaken);
+
+        var actual = dateExtraction.getDateTaken(test_mp4_location);
 
         assertEquals(expected, actual);
     }
